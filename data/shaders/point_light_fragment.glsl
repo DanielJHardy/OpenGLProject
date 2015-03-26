f5b0d2f92486fa1ea8c12a5babeac98765de4776
Daniel Hardy <flamingarro@hotmail.com>
2015-03-26 16:50:29 +1100

    added Specular. Not very smooth

--- data/shaders/point_light_fragment.glsl
+++ data/shaders/point_light_fragment.glsl
@@ -9,8 +9,12 @@ uniform float light_radius;
 uniform sampler2D position_texture;
 uniform sampler2D normal_texture;
 
+float specPower;
+
 void main()
 {
+	specPower = 10;
+
 	//compute texture coord
 	vec2 texcoord = gl_FragCoord.xy / textureSize(position_texture,0).xy;
 
@@ -27,11 +31,17 @@ void main()
 	vec3 L = normalize(to_light);
 
 	//compute lambertian term
-	float d = max(0, dot(normal_sample, L));
+	float d = max(0.0f, dot(normal_sample, L));
 
 	//compute falloff
-	float falloff = 1 - min(1,(length(to_light) / light_radius));
+	float falloff = 1.0f - min(1.0f,(length(to_light) / light_radius));
+
+	//specular
+	vec3 E = normalize(position_sample);
+	vec3 R = reflect(L,normal_sample.xyz);
+	float s = max(0.0f, dot(E,R));
+	s = pow(s, specPower);
 
 	//output lambert * fallof * color
-	light_output = d * falloff * light_diffuse;
+	light_output = (d * falloff * light_diffuse) + (falloff * light_diffuse) * s;
 }
